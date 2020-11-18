5c90b737e00b6a98f39d84ea93f5b90b2b62a20a
Narek <narekb91@gmail.com>
2020-11-18 08:26:13 +0400

    Logical issue fix related to cancelled policies

--- PoliciesTest/PolicyEventStream.swift
+++ PoliciesTest/PolicyEventStream.swift
@@ -33,25 +33,32 @@ extension PolicyEventStream {
     private func populateData() {
         if let events = events {
             
+            let cancelEvents = events.filter({ $0.type == .policyCancelled })
+            var updatedEvents = [PolicyEvent]()
+            events.filter({ $0.type != .policyCancelled }).forEach { (event) in
+                if let cancelled = cancelEvents.first(where: { $0.payload.policyId == event.payload.policyId }) {
+                    var event = event
+                    event.payload.endDate = cancelled.payload.timestamp
+                    updatedEvents.append(event)
+                } else {
+                    updatedEvents.append(event)
+                }
+            }
+            
             /** Getting vehicles from json data structure **/
-            let vehicles = Array(Set(events.compactMap({ $0.payload.vehicle })))
+            let vehicles = Array(Set(updatedEvents.compactMap({ $0.payload.vehicle })))
             let cars = vehicles.map { (vehicle) -> Car in
-                                                
+                
                 /** Create policies for every vehicle **/
-                let policies = events.filter({ $0.payload.vehicle == vehicle && $0.type == .policyCreated })
+                let policies = updatedEvents.filter({ $0.payload.vehicle == vehicle && $0.type == .policyCreated })
                     .map({ Policy($0.payload.policyId, events: [$0]) })
                 
-                /** updated previously created policies based on policy event type **/
-                events.filter({ $0.type != .policyCreated }).forEach { (event) in
-                    switch event.type {
-                    case .policyCancelled:
-                        policies.first(where: { $0.id == event.payload.policyId })?.setEndDate(event.payload.endDate) 
-                    case .policyExtension:
-                        policies.first(where: { $0.id == event.payload.originalPolicyId })?.events.append(event)
-                    default: break
-                    }
+                /** updated previously created policies based on policy extension **/
+                updatedEvents.filter({ $0.type == .policyExtension }).forEach { (event) in
+                    policies.first(where: { $0.id == event.payload.originalPolicyId })?
+                        .events.append(event)
                 }
-                
+
                 /** for every vehicle I create car object with his related policies **/
                 let car = Car(vehicle, policies: policies)
                 return car
